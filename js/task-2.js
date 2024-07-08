2508f9b2baa1d2b5f58d43ad94a25a9f486aceb1
Артем Томащук <tomashhukartem@gmail.com>
2024-07-08 20:15:32 +0300

    change js code

--- js/task-2.js
+++ js/task-2.js
@@ -2,7 +2,7 @@
 
 function getShippingMessage(country, price, deliveryFee) {
     const totalPrice = price + deliveryFee;
-    const message = "Shipping to ${country} will cost ${totalPrice} credits";
+    const message = `Shipping to ${country} will cost ${totalPrice} credits`;
     return message;
 }
 
